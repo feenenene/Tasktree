10a1d7166c6125ca0a194ad04d0af36770f49f02
kajiayano <ayanyano428@gmail.com>
2019-01-11 14:13:47 +0900

    add logout and admin user

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,12 +3,14 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
+  belongs_to :user
+
   private
 
   def set_nameless_name
     self.name = '名前なし' if name.blank?
   end
-  
+
   def validate_name_not_including_comma
     errors.add(:name,'にカンマを含めることはできません') if name&.include?(',')
   end
